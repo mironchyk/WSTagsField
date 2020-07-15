6f36c35427dd8a4d9e23509ec7c39c1a302b7e40
Gena Mironchyk <g.mironchyk@gmail.com>
2020-07-15 13:01:07 +0300

    Added multiple selection textfield

--- Source/WSTagView.swift
+++ Source/WSTagView.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 open class WSTagView: UIView, UITextInputTraits {
-
-    fileprivate let textLabel = UILabel()
+    
+    let textLabel = UILabel()
     fileprivate let closeButton = UIButton(type: .custom)
     
     open var displayText: String = "" {
@@ -21,35 +21,35 @@ open class WSTagView: UIView, UITextInputTraits {
     }
     
     open var closeButtonImage : UIImage?
-
+    
     open var displayDelimiter: String = "" {
         didSet {
             updateLabelText()
             setNeedsDisplay()
         }
     }
-
+    
     open var font: UIFont? {
         didSet {
             textLabel.font = font
             setNeedsDisplay()
         }
     }
-
+    
     open var cornerRadius: CGFloat = 3.0 {
         didSet {
             self.layer.cornerRadius = cornerRadius
             setNeedsDisplay()
         }
     }
-
+    
     open var borderWidth: CGFloat = 0.0 {
         didSet {
             self.layer.borderWidth = borderWidth
             setNeedsDisplay()
         }
     }
-
+    
     open var borderColor: UIColor? {
         didSet {
             if let borderColor = borderColor {
@@ -58,43 +58,47 @@ open class WSTagView: UIView, UITextInputTraits {
             }
         }
     }
-
+    
     open var tagViewBackgroundColor : UIColor! {
         didSet { updateContent(animated: false) }
     }
     
-
+    
     /// Background color to be used for selected state.
     open var selectedColor: UIColor? {
         didSet { updateContent(animated: false) }
     }
-
+    
     open var textColor: UIColor? {
         didSet { updateContent(animated: false) }
     }
-
+    
     open var selectedTextColor: UIColor? {
         didSet { updateContent(animated: false) }
     }
-
+    
     internal var onDidRequestDelete: ((_ tagView: WSTagView, _ replacementText: String?) -> Void)?
     internal var onDidRequestSelection: ((_ tagView: WSTagView) -> Void)?
     internal var onDidInputText: ((_ tagView: WSTagView, _ text: String) -> Void)?
-
+    
     open var selected: Bool = false {
         didSet {
-            if selected && !isFirstResponder {
-                _ = becomeFirstResponder()
-            }
-            else if !selected && isFirstResponder {
-                _ = resignFirstResponder()
+            if !allowsMultipleSelection {
+                if selected && !isFirstResponder {
+                    _ = becomeFirstResponder()
+                }
+                else if !selected && isFirstResponder {
+                    _ = resignFirstResponder()
+                }
             }
             updateContent(animated: true)
         }
     }
-
+    open var allowsMultipleSelection: Bool = false
+    open var removable: Bool = true
+    
     // MARK: - UITextInputTraits
-
+    
     public var autocapitalizationType: UITextAutocapitalizationType = .none
     public var autocorrectionType: UITextAutocorrectionType  = .no
     public var spellCheckingType: UITextSpellCheckingType  = .no
@@ -103,28 +107,29 @@ open class WSTagView: UIView, UITextInputTraits {
     public var returnKeyType: UIReturnKeyType = .next
     public var enablesReturnKeyAutomatically: Bool = false
     public var isSecureTextEntry: Bool = false
-
+    
     // MARK: - Initializers
-
+    
     public init(tag: WSTag, closeButtonImage : UIImage? = nil) {
         super.init(frame: CGRect.zero)
         self.closeButtonImage = closeButtonImage
         self.backgroundColor = tintColor
         self.layer.cornerRadius = cornerRadius
         self.layer.masksToBounds = true
-
+        
         textColor = .white
         selectedColor = .gray
         selectedTextColor = .black
-
+        
         textLabel.frame = CGRect(x: layoutMargins.left, y: layoutMargins.top, width: 0, height: 0)
         textLabel.font = font
         textLabel.textColor = .white
         addSubview(textLabel)
-
+        
         self.displayText = tag.text
         updateLabelText()
-
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapGestureRecognizer))
         addGestureRecognizer(tapRecognizer)
         
@@ -142,25 +147,25 @@ open class WSTagView: UIView, UITextInputTraits {
     @objc private func deleteButtonAction () {
         onDidRequestDelete?(self, nil)
     }
-
+    
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         assert(false, "Not implemented")
     }
-
+    
     // MARK: - Styling
-
+    
     fileprivate func updateColors() {
         self.backgroundColor = selected ? selectedColor : tagViewBackgroundColor
         textLabel.textColor = selected ? selectedTextColor : textColor
     }
-
+    
     internal func updateContent(animated: Bool) {
         guard animated else {
             updateColors()
             return
         }
-
+        
         UIView.animate(
             withDuration: 0.2,
             animations: { [weak self] in
@@ -178,20 +183,20 @@ open class WSTagView: UIView, UITextInputTraits {
             }
         )
     }
-
+    
     // MARK: - Size Measurements
-
+    
     open override var intrinsicContentSize: CGSize {
         let labelIntrinsicSize = textLabel.intrinsicContentSize
         if self.closeButtonImage != nil {
             let buttonSize = closeButton.intrinsicContentSize
             return CGSize(width: labelIntrinsicSize.width + buttonSize.width + layoutMargins.left + layoutMargins.right,
-                                 height: labelIntrinsicSize.height + layoutMargins.top + layoutMargins.bottom)
+                          height: labelIntrinsicSize.height + layoutMargins.top + layoutMargins.bottom)
         }
         return CGSize(width: labelIntrinsicSize.width + layoutMargins.left + layoutMargins.right,
                       height: labelIntrinsicSize.height + layoutMargins.top + layoutMargins.bottom)
     }
-
+    
     open override func sizeThatFits(_ size: CGSize) -> CGSize {
         let layoutMarginsHorizontal = layoutMargins.left + layoutMargins.right
         let layoutMarginsVertical = layoutMargins.top + layoutMargins.bottom
@@ -201,7 +206,7 @@ open class WSTagView: UIView, UITextInputTraits {
         return CGSize(width: labelSize.width + layoutMarginsHorizontal,
                       height: labelSize.height + layoutMarginsVertical)
     }
-
+    
     open func sizeToFit(_ size: CGSize) -> CGSize {
         if intrinsicContentSize.width > size.width {
             return CGSize(width: size.width,
@@ -209,7 +214,7 @@ open class WSTagView: UIView, UITextInputTraits {
         }
         return intrinsicContentSize
     }
-
+    
     // MARK: - Attributed Text
     fileprivate func updateLabelText() {
         // Unselected shows "[displayText]," and selected is "[displayText]"
@@ -218,59 +223,59 @@ open class WSTagView: UIView, UITextInputTraits {
         let intrinsicSize = self.intrinsicContentSize
         frame = CGRect(x: 0, y: 0, width: intrinsicSize.width, height: intrinsicSize.height)
     }
-
+    
     // MARK: - Laying out
     open override func layoutSubviews() {
         super.layoutSubviews()
         textLabel.frame = bounds.inset(by: layoutMargins)
         if self.closeButtonImage != nil {
-             closeButton.frame = CGRect(x: textLabel.frame.origin.x + textLabel.frame.size.width - 15, y: layoutMargins.top, width: self.closeButtonImage!.size.width, height: self.closeButtonImage!.size.height)
+            closeButton.frame = CGRect(x: textLabel.frame.origin.x + textLabel.frame.size.width - 15, y: layoutMargins.top, width: self.closeButtonImage!.size.width, height: self.closeButtonImage!.size.height)
         }
-       
+        
         if frame.width == 0 || frame.height == 0 {
             frame.size = self.intrinsicContentSize
         }
     }
-
+    
     // MARK: - First Responder (needed to capture keyboard)
     open override var canBecomeFirstResponder: Bool {
         return true
     }
-
+    
     open override func becomeFirstResponder() -> Bool {
         let didBecomeFirstResponder = super.becomeFirstResponder()
         selected = true
         return didBecomeFirstResponder
     }
-
+    
     open override func resignFirstResponder() -> Bool {
         let didResignFirstResponder = super.resignFirstResponder()
         selected = false
         return didResignFirstResponder
     }
-
+    
     // MARK: - Gesture Recognizers
     @objc func handleTapGestureRecognizer(_ sender: UITapGestureRecognizer) {
-        if selected {
+        if selected && !allowsMultipleSelection {
             return
         }
         onDidRequestSelection?(self)
     }
-
+    
 }
 
 extension WSTagView: UIKeyInput {
-
+    
     public var hasText: Bool {
         return true
     }
-
+    
     public func insertText(_ text: String) {
         onDidInputText?(self, text)
     }
-
+    
     public func deleteBackward() {
         onDidRequestDelete?(self, nil)
     }
-
+    
 }
